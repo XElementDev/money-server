76d30f43da1a557d018bbf1ce6b7413edbb1c4b6
IanStorm <IanStorm28@outlook.com>
2018-11-13 21:14:44 +0100

    Migrated `tsoa` task to Gulp4

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,38 @@
+import * as cpp from "child-process-promise";
 import * as gulp from "gulp";
 import * as sourcemaps from "gulp-sourcemaps";
 import * as ts from "gulp-typescript";
 import * as path from "path";
+import * as through2 from "through2";
+import * as File from "vinyl";
 
 
 const srcFolderName = "src";
 const allFolders = "**";
+
+
+const tsoaTaskName: string = "_tsoa";
+
+const tsoaFileGlobs: Array<string> = [
+	path.join(srcFolderName, allFolders, "tsoa.json")
+];
+
+const tsoaTaskFunction: () => Promise<void> = async () => {
+	const folderPathPromise = new Promise<string>((resolve, __) => {
+		gulp.src(tsoaFileGlobs)
+			.pipe(through2.obj(async (file: File, __, cb: through2.TransformCallback) => {
+				resolve(path.dirname(file.path));
+				cb(null, file);
+			}))
+		;
+	});
+	const folderPath = await folderPathPromise;
+	await cpp.exec("tsoa swagger", { cwd: folderPath });
+	await cpp.exec("tsoa routes", { cwd: folderPath });
+	return;
+};
+
+gulp.task(tsoaTaskName, tsoaTaskFunction);
 
 
 const typescriptTaskName: string = "_ts";
@@ -38,6 +65,7 @@ const buildTaskName: string = "build";
 gulp.task(
 	buildTaskName, 
 	gulp.series(
+		tsoaTaskName,
 		typescriptTaskName
 	)
 );
